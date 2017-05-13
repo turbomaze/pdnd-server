06d94bbf650a27e4ec9f1e064e7d4c2075fed836
Anthony Liu <anthonyscottliu@gmail.com>
2017-05-13 18:50:08 -0400

    Remove run.sh from repo.

--- utils.js
+++ utils.js
@@ -1,3 +1,33 @@
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.SENDER_PASSWORD
+  }
+});
+
+function sendAlarmEmail(message, address) {
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
+    to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ?', // plain text body
+    html: '<b>Hello world ?</b>' // html body
+  };
+  
+  //end mail with defined transport object
+  trsporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(
+      'Message %s sent: %s', info.messageId, info.response
+    );
+  });
+}
+
 module.exports = {
   actuallyParseBody: function(s) {
     return s.split('&').reduce(function(a, keyValue) {
